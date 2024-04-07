d0e63ae8882cbd25c36426cba93a553d7c69ad33
Adwaer <adwaer@live.ru>
2024-04-08 01:10:29 +0200

    small updates

--- scripts/deployEarnContract.ts
+++ scripts/deployEarnContract.ts
@@ -1,18 +1,24 @@
 import { toNano } from '@ton/core';
 import { EarnContract } from '../wrappers/EarnContract';
 import { NetworkProvider } from '@ton/blueprint';
+import { Blockchain } from '@ton/sandbox';
+
+const getRandomId = () => BigInt(Math.floor(Math.random() * 10000));
 
 export async function run(provider: NetworkProvider) {
-    const earnContract = provider.open(await EarnContract.fromInit());
+    const blockchain = await Blockchain.create();
+    const [founder] = await blockchain.createWallets(1);
+
+    const earnContract = provider.open(await EarnContract.fromInit(getRandomId(), toNano(1), founder.address));
 
     await earnContract.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: toNano('0.1')
         },
         {
             $$type: 'Deploy',
-            queryId: 0n,
+            queryId: 0n
         }
     );
 
--- tests/EarnContract.spec.ts
+++ tests/EarnContract.spec.ts
@@ -3,6 +3,8 @@ import { toNano } from '@ton/core';
 import { EarnContract } from '../wrappers/EarnContract';
 import '@ton/test-utils';
 
+const getRandomId = () => BigInt(Math.floor(Math.random() * 10000));
+
 describe('EarnContract', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
@@ -11,7 +13,8 @@ describe('EarnContract', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        earnContract = blockchain.openContract(await EarnContract.fromInit());
+        const [founder] = await blockchain.createWallets(1);
+        earnContract = blockchain.openContract(await EarnContract.fromInit(getRandomId(), toNano(1), founder.address));
 
         deployer = await blockchain.treasury('deployer');
 
@@ -37,5 +40,10 @@ describe('EarnContract', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and earnContract are ready to use
+    });
+
+    it('should return min deposit', () => {
+        const minDeposit = earnContract.getMinDeposit();
+        console.log('minDeposit', minDeposit);
     });
 });
