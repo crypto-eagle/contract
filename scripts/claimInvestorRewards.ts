44b07b81cdcd04c340b2738e7a05d5b00ee751f4
Azat Khairutdinov <ahairutdinov@direct-credit.ru>
2024-04-21 22:36:57 +0200

    Refactor

---
+++ scripts/claimInvestorRewards.ts
@@ -0,0 +1,33 @@
+import { Address, fromNano, toNano } from '@ton/core';
+import { NetworkProvider } from '@ton/blueprint';
+import { EarnContract } from '../build/EarnContract/tact_EarnContract';
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const ui = provider.ui();
+
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Earn Contract address'));
+
+    if (!(await provider.isContractDeployed(address))) {
+        ui.write(`Error: Contract at address ${address} is not deployed!`);
+
+        return;
+    }
+
+    const value = toNano(await ui.input('Value'));
+
+    const earnContract = provider.open(EarnContract.fromAddress(address));
+
+    const result = await earnContract.send(
+        provider.sender(),
+        {
+            value: value,
+            bounce: true
+        },
+        {
+            $$type: "ClaimRewards"
+        }
+    );
+
+    ui.clearActionPrompt();
+    console.log('result', result);
+}
