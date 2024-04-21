b25409234e425b6c5f79ec95f43a524b076919c0
Adwaer <adwaer@live.ru>
2024-04-21 22:06:52 +0200

    founder claim rewards

---
+++ scripts/claim-rewards.ts
@@ -0,0 +1,33 @@
+import { Address, toNano } from '@ton/core';
+import { NetworkProvider } from '@ton/blueprint';
+import { FounderContract } from '../build/FounderContract/tact_FounderContract';
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const ui = provider.ui();
+
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Founder Contract address'));
+
+    if (!(await provider.isContractDeployed(address))) {
+        ui.write(`Error: Contract at address ${address} is not deployed!`);
+
+        return;
+    }
+
+    const value = await ui.input('Claim value');
+
+    const founderContract = provider.open(FounderContract.fromAddress(address));
+
+    const result = await founderContract.send(
+        provider.sender(),
+        {
+            value: toNano(value),
+            bounce: true
+        },
+        {
+            $$type: "ClaimStakeHoldersRewards"
+        }
+    );
+
+    ui.clearActionPrompt();
+    console.log('result', result);
+}
